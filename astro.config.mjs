b4494db9c12b57aa0d240fecbb42d1ca133c772c
jcu25 <jcuy@mcm.edu.ph>
2024-08-14 11:06:03 +0800

    fix: fixing production site not working; added base to hrefs, corrected base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jcu25.github.io',
-  base: "jcu25.github.io/public"
+  base: "/jcu25.github.io/"
 });
