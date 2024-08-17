d9d32275486ad4c14d44e94045fbd10b63c2038c
jcu25 <jcuy@mcm.edu.ph>
2024-08-17 20:00:04 +0800

    fix: removed base from astro config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jcu25.github.io',
-  base: "/jcu25.github.io/"
+  site: 'https://jcu25.github.io'
 });
